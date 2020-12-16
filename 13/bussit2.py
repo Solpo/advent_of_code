02b6f755368750783e4c6655a5167979738e7975
Solpo <3440817+Solpo@users.noreply.github.com>
2020-12-16 22:42:32 +0200

    Nyt on hieno

--- 13/bussit2.py
+++ 13/bussit2.py
@@ -1,5 +1,3 @@
-import os
-
 def lue_aikataulu(tiedosto) -> (int, list):
     with open(tiedosto) as f:
         f.readline()
@@ -8,37 +6,32 @@ def lue_aikataulu(tiedosto) -> (int, list):
 
 bussit = lue_aikataulu("aikataulu.txt")
 
-print(f"Bussit: {bussit}")
+# print(f"Bussit: {bussit}")
 
 lahtoajat_vuorovalit = []
 for i in range(len(bussit)):
     if bussit[i] != "x":
         lahtoajat_vuorovalit.append((i, int(bussit[i])))
-        print(f"indeksi: {i}, bussi {bussit[i]}")
-
-lahtoajat_vuorovalit.sort(key=lambda x: x[1], reverse=True)
-print(lahtoajat_vuorovalit)
+        # print(f"indeksi: {i}, bussi {bussit[i]}")
 
 
-# ajan_nollapiste = lahtoajat_vuorovalit[0][1] - lahtoajat_vuorovalit[0][0]
-ajan_nollapiste = 99999999999885 - 50
-
-def matsaako_bussit_minuutilla(bussi: int, lahtominuutti: int) -> bool:
+def matsaako(bussi: int, lahtominuutti: int) -> bool:
     viive, vuorovali = lahtoajat_vuorovalit[bussi]
     if (lahtominuutti + viive) % vuorovali == 0:
-        if bussi >= len(lahtoajat_vuorovalit) - 1:
-            return True
-        else:
-            return matsaako_bussit_minuutilla(bussi + 1, lahtominuutti)
+        return True
+    return False
+
+ajan_nollapiste = 0
+isoin_matsannut = -1
+kasvatus = 1
+
+while isoin_matsannut < len(lahtoajat_vuorovalit) - 1:
+    # print(f"testataan lähtöaikaa {ajan_nollapiste}")
+    if not matsaako(isoin_matsannut + 1, ajan_nollapiste):
+        ajan_nollapiste += kasvatus
     else:
-        return False
-while not matsaako_bussit_minuutilla(0, ajan_nollapiste):
-    ajan_nollapiste += lahtoajat_vuorovalit[0][1]
+        isoin_matsannut += 1
+        kasvatus *= lahtoajat_vuorovalit[isoin_matsannut][1]
 
 print("oikea vastaus")
 print(ajan_nollapiste)
-with open("vastaus.txt", "w") as vastaus:
-    vastaus.write("Vastaus on ")
-    vastaus.write(ajan_nollapiste)
-
-os.system("shutdown /s /t 60")
